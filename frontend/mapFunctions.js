e889d4f2355e79eba0c627534b912b3654f89fec
Mervin Lavin <mervin.lavin@gmail.com>
2015-09-04 14:58:27 +0800

    Added table updating.

--- frontend/mapFunctions.js
+++ frontend/mapFunctions.js
@@ -8,6 +8,7 @@ function initialize() {
   for (i = 0; i < siteData.length; i++) {
     addMarker(map,siteData[i]);
   }
+  updateTable(siteData);
 }
 
 function initMap() {
@@ -24,8 +25,8 @@ function initMap() {
 function getSiteData()
 {
   var site = [
-    {name:"Site 1", longitude:14.58, latitude:121.00, type:"LTE", operator:"Globe"},
-    {name:"Site 2", longitude:14.63, latitude:121.03, type:"3G", operator:"Smart"}
+    {name:"Site 1", longitude:14.58, latitude:121.00, technology:"LTE", type:"Flexi", operator:"Globe"},
+    {name:"Site 2", longitude:14.63, latitude:121.03, technology:"3G", type:"Flexi", operator:"Smart"}
   ];
   return site;
 }
@@ -62,4 +63,18 @@ function showSiteData(marker,site) {
   map.setZoom(10);
   map.setCenter(marker.getPosition());
   infoWindow.open(map,marker);
+}
+
+function updateTable(siteData) {
+  $("#maps table tbody").empty();
+  var tableData = "";
+  for (i = 0; i < siteData.length; i++) {
+    tableData = "<tr>"
+      + "<td>" + siteData[i].name + "</td>"
+      + "<td>" + siteData[i].technology + "</td>"
+      + "<td>" + siteData[i].type + "</td>"
+      + "<td>" + siteData[i].operator + "</td>"
+      + "</tr>";
+      $("#maps table").append(tableData);
+    }
 }
